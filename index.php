bba00c61faeb839337264e563a6dcc200865026a
DennisGithubAccount <112478922+DennisGithubAccount@users.noreply.github.com>
2022-11-02 13:21:42 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" >
+	<!-- Gilroy-Bold import -->
+
+
+	
+	</style>
+		
+		<!-- div class used to center text and create a doc border.-->
+		<div class="div">
+
+<center>
+	<div>
+<
+<body>
+	
+
+	<h1 >test</h1>
+	
+	
+</body>
+
+
+</center>
+</footer>
+</div>
+</html>
