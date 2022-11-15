85b2bbe5984f820f150aa4bb4db1c54771bb8a10
DennisGithubAccount <112478922+DennisGithubAccount@users.noreply.github.com>
2022-11-15 10:07:00 +0100

    r

--- h.php
+++ h.php
@@ -1,1 +1,10 @@
-<h1>Hello</H1>
+<?php
+    $to      = 'dennisspruijt2005@gmail.com';
+    $subject = 'the subject';
+    $message = 'hello';
+    $headers = 'From: webmaster@example.com'       . "\r\n" .
+                 'Reply-To: webmaster@example.com' . "\r\n" .
+                 'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
+?>
