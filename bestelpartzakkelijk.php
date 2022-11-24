095bade075eb18ee717f66341f4d435285d9d6ac
DennisGithubAccount <112478922+DennisGithubAccount@users.noreply.github.com>
2022-11-24 17:23:24 +0100

    rrg
    rgfd

--- bestelpartzakkelijk.php
+++ bestelpartzakkelijk.php
@@ -1,22 +1,52 @@
 <?php
-  $bedrijfnaam=$_POST['voornaam'];
-  $telef=$_POST['telef'];
-  $email=$_POST['email'];
-  $bedrag=$_POST['hoeveelheid'];
-  
-  $subject= "U heeft een bestelling geplaatst";
-  $body = "Geachte" . $bedrijfnaam .  "\r\nU" . "bestelling is compleet:\r\n\r\n Het totale bedrag is" . $bedrag.
-  "\r\nDe" . " bestelling wordt binnen 5 dagen bezorgd." ; 
-  
-  if(mail($email, $subject, $body)){
-      echo "<script>alert('Het is gelukt om een bestelling te plaatsen!')</script>";
-      ?>
-      <META HTTP-EQUIV="Refresh" CONTENT="0; URL=bestelpagina.php">
-      <?php
-  }else{
-      echo "<script>alert('Sorry, bestellling is niet gelukt')</script>";
-      ?>
-      <META HTTP-EQUIV="Refresh" CONTENT="0; URL=bestelpagina.php">
-      <?php
-  }
+include("navbar.php");
+include("bestelpartzakkelijk.html");
+include("DBconfig.php");
+if(isset($_POST["submit"])){
+$melding="";
+$BedrijfNaam= htmlspecialchars($_POST['Naam']);
+$klant = $Naam . ;
+$locatie = htmlspecialchars($_POST['locatie']);
+$email = htmlspecialchars($_POST['email']);
+
+
+$hoeveelheid = filter_var($_POST['integer']);
+
+$sql = "SELECT * FROM 'klant' WHERE ID = ?";
+$stmt = $verbinding ->prepare($sql);
+$resultaat = $stmt -> fetch(PDO::FETCH_ASSOC);
+
+
+
+if($hoeveelheid>24){
+    $melding="Onjuiste aantal biertjes";
+    echo"<div id='melding'>".$melding."</div>";   
+}
+else{
+if($resultaat){
+    $melding = "Unknown error"; 
+    
+}else{
+    $sql ="INSERT INTO particulier (ID, Naam, locatie, email,  hoeveelheid) values (null,?,?,?,?,?,?,?)";
+    $stmt = $verbinding ->prepare($sql);
+try{
+    $stmt->execute(array(
+        $BedrijfNaam,
+        $klant,
+        $locatie,
+        $email,
+        $hoeveelheid
+    ));
+    $melding ="Bestelling geplaats. Uw ontvang een mailtje binnen enkelen minuten";
+
+}   catch(PDOException $e){
+    $melding="Kon geen bestelling maken." . $e->getMessage();
+
+
+} 
+echo"<div id='melding'>".$melding."</div>";
+}
+}
+}
+
 ?>
